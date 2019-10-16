2399d56763563aa20c1aa529d6df052b86952574
Jeff Cleverley <jeff@jeffcleverley.com>
2019-10-16 20:29:48 +0800

    first commit

---
+++ gridpane-disable-rss/gridpane-disable-rss.php
@@ -0,0 +1,17 @@
+<?php
+add_action('do_feed_rdf', 'gridpane_security_disable_feed', 1);
+add_action('do_feed_rss', 'gridpane_security_disable_feed', 1);
+add_action('do_feed_rss2', 'gridpane_security_disable_feed', 1);
+add_action('do_feed_atom', 'gridpane_security_disable_feed', 1);
+add_action('do_feed_rss2_comments', 'gridpane_security_disable_feed', 1);
+add_action('do_feed_atom_comments', 'gridpane_security_disable_feed', 1);
+add_filter('the_generator','gridpane_security_remove_wp_version_rss');
+
+function gridpane_security_remove_wp_version_rss() {
+	return '';
+}
+
+function gridpane_security_disable_feed() {
+	wp_die( __( 'GridPane Security has disabled the RSS Feed, please visit the <a href="'. esc_url( home_url( '/' ) ) .'">homepage</a>!' ) );
+}
+
---
+++ mu-gridpane-disable-rss.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * GridPane Disable RSS
+ *
+ * @package         GridPane\GridPaneDisableRSS
+ * @since           0.1.0
+ * @author          Jeff Cleverley
+ * @license         GNU-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:     GridPane Disable XMLRPC
+ * Plugin URI:      https://gitlab.gridpane.net/jeff/KillRSS
+ * Description:     Disable XMLRPC to mitigate DDoS
+ * Version:         0.1.0
+ * Author:          Jeff Cleverley
+ * Author URI:      https://gitlab.gridpane.net/jeff
+ * Author URI:      https://gitlab.gridpane.net/jeff
+ * License:         GPL-2.0+
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+require __DIR__ . '/gridpane-disable-rss/gridpane-disable-rss.php';
+
